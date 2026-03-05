394ce6ef64bc0259ddd26806fdeffa787c5cc3fa
zizoyazan18-ctrl <zizoyazan18@gmail.com>
2026-03-06 00:11:09 +0200

    update html and js

--- script.js
+++ script.js
@@ -1,10 +1,4 @@
-<<<<<<< HEAD
 document.getElementById("btn").addEventListener("click", () => {
     const msg = document.getElementById("msg");
     msg.innerText = "Hello from JavaScript! Your site is interactive ✅";
-=======
-document.getElementById("btn").addEventListener("click", () => {
-    const msg = document.getElementById("msg");
-    msg.innerText = "Hello from JavaScript! Your site is interactive ✅";
->>>>>>> 459d964 (Initial commit)
   });
